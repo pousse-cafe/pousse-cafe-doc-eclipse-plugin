968262b9e53a3223c1b5140247dd2b7b38a445b0
Gérard Dethier <info@gerarddethier.be>
2024-07-09 17:12:17 +0200

    Initial commit.

---
+++ copy_jar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ../pousse-cafe-doc/target/*.jar .
+cp ~/.m2/repository/org/freemarker/freemarker/2.3.30/freemarker-2.3.30.jar .
+cp ~/.m2/repository/org/xhtmlrenderer/flying-saucer-core/9.1.20/flying-saucer-core-9.1.20.jar .
+cp ~/.m2/repository/org/xhtmlrenderer/flying-saucer-pdf/9.1.20/flying-saucer-pdf-9.1.20.jar .
+cp ~/.m2/repository/com/lowagie/itext/2.1.7/itext-2.1.7.jar .
